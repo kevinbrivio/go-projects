bdc09b97cbc408b47bd947035d23792a4f05807b
kevinbriv <kevin.brivio@helios.id>
2026-08-15 22:31:02 +0700

    Squashed 'task-tracker/' content from commit 081396d
    git-subtree-dir: task-tracker
    git-subtree-split: 081396dc6fc3d3ae2476d9a6f99966e9395d2201

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	// read the line from runtime
+	reader := bufio.NewReader(os.Stdin)
+
+	// Listen for every time control+c was typed
+	sigChan := make(chan os.Signal, 1)
+
+	// If the OS sends SIGINT (ctrl+C) or SIGTERM
+	// We might need to drop it into sigChan instead of killing the program
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("\ntask-cli is abandoned, goodbye...")
+		os.Exit(0)
+	}()
+
+	// Init the Task
+	taskList := TaskList{}
+
+	for {
+		fmt.Print("task-cli ")
+
+		line, _ := reader.ReadString('\n') // Read until we got newline a.k.a enter
+		line = strings.TrimSpace(line)
+
+		if line == "" {
+			continue
+		}
+
+		if line == "exit" {
+			break
+		}
+		parts := strings.SplitN(line, " ", 3)
+		command := parts[0]
+		description := ""
+
+		if len(parts) > 1 {
+			description = parts[1]
+		}
+
+		switch command {
+		case "add":
+			if description == "" {
+				fmt.Println("Oh no, what do you want to add...?")
+				fmt.Println("Use this format: add [task]")
+				continue
+			}
+
+			id, err := taskList.addTask(description)
+			if err != nil {
+				break
+			}
+			fmt.Printf("Task added successfully (ID: %d)\n", id)
+
+		case "list":
+			// empty description means list all tasks
+			switch description {
+			case "":
+				taskList.showTasks("")
+			case "done":
+				taskList.showTasks(StatusDone)
+			case "todo":
+				taskList.showTasks(StatusTodo)
+			case "in-progress":
+				taskList.showTasks(StatusInProgress)
+			}
+
+		case "mark-in-progress":
+			targetID := description
+			if (targetID == "") {
+				fmt.Println("Please provide task ID")
+				continue
+			}
+			
+			if err := taskList.MarkTask(targetID, "in-progress"); err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf("Task with ID: %s successfully marked to in-progress\n", targetID)
+		case "mark-done":
+			targetID := description
+			if (targetID == "") {
+				fmt.Println("Please provide task ID")
+				continue
+			}
+			
+			if err := taskList.MarkTask(targetID, "done"); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Task with ID: %s successfully marked to done\n", targetID)
+
+		case "update":
+			if len(parts) < 3 {
+				fmt.Println("Use this format: update [id] [task]")
+				continue
+			}
+
+			targetID := parts[1]
+
+			description = parts[2]
+			if err := taskList.UpdateTask(targetID, description); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Task with ID: %s successfully updated\n", targetID)
+
+		case "delete":
+			targetID := description
+			if (targetID == "") {
+				fmt.Println("Please provide task ID")
+				continue
+			}
+			
+			if err := taskList.DeleteTask(targetID); err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Printf("Task with ID: %s successfully deleted\n", targetID)
+
+		}
+	}
+}
---
+++ tasks.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type Status string
+
+const (
+	StatusTodo       Status = "todo"
+	StatusInProgress Status = "in-progress"
+	StatusDone       Status = "done"
+)
+
+type Task struct {
+	ID          int       `json:"id"`
+	Description string    `json:"description"`
+	Status      Status    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type TaskList struct {
+	Tasks []Task
+}
+
+/// Add task into TaskList struct
+func (tl *TaskList) addTask(description string) (int, error) {
+	num := fmt.Sprintf("%d", len(tl.Tasks)+1)
+	id, err := strconv.Atoi(num)
+	if err != nil {
+		fmt.Println("Cannot convert ID: ", err)
+		return -1, err
+	}
+
+	newTask := Task{
+		ID:          id,
+		Description: description,
+		Status:      StatusTodo,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	tl.Tasks = append(tl.Tasks, newTask)
+
+	return id, nil
+}
+
+/// Show tasks
+func (tl *TaskList) showTasks(status Status) error {
+	if (len(tl.Tasks) == 0) {
+		fmt.Println("No tasks was found.")
+		return nil
+	}
+	// Sort by UpdatedAt
+	sort.Slice(tl.Tasks, func(i, j int) bool {
+		return tl.Tasks[i].UpdatedAt.After(tl.Tasks[j].UpdatedAt)
+	})
+
+	found := false
+
+	fmt.Println("\n========================================================")
+	for _, t := range tl.Tasks {
+		if status != "" && t.Status != status {
+			continue
+		}
+
+		found = true
+
+		fmt.Printf(
+			"ID: %d | %s | %s | updated: %s\n",
+			t.ID,
+			t.Status,
+			t.Description,
+			t.UpdatedAt.Format("2006-01-02 15:04"),
+		)
+	}
+	if !found {
+		fmt.Printf("no %s tasks was found...\n", status)
+	}
+	fmt.Println("========================================================")
+
+	return nil
+}
+
+// GetTaskByID returns the task with given ID
+func (tl *TaskList) GetTaskByID(ID int) (*Task, error) {
+	for i := range tl.Tasks {
+		if tl.Tasks[i].ID == ID {
+			return &tl.Tasks[i], nil
+		}
+	}
+
+	return nil, fmt.Errorf("task with id %d not found", ID)
+}
+
+// MarkTask return specific task by target status
+func (tl *TaskList) MarkTask(ID string, status string) (error) {
+	targetID, err := strconv.Atoi(ID)
+	if err != nil {
+		return fmt.Errorf("wrong id")
+	}
+
+	task, err := tl.GetTaskByID(targetID)
+	if err != nil {
+		return err
+	}
+
+	task.Status = Status(status)
+	task.UpdatedAt = time.Now()
+
+	return nil
+}
+
+// UpdateTask only updates the description and updated_at
+func (tl *TaskList) UpdateTask(ID string, description string) (error) {
+	targetID, err := strconv.Atoi(ID)
+	if err != nil {
+		return fmt.Errorf("wrong id")
+	}
+
+	task, err := tl.GetTaskByID(targetID)
+	if err != nil {
+		return err
+	}
+
+	task.Description = description
+	task.UpdatedAt = time.Now()
+
+	return nil
+}
+
+// DeleteTask will delete task permanently
+func (tl *TaskList) DeleteTask(ID string) (error) {
+	targetID, err := strconv.Atoi(ID)
+	if err != nil {
+		return fmt.Errorf("wrong id")
+	}
+	
+	for i, task := range tl.Tasks {
+		if task.ID == targetID {
+			tl.Tasks = append(tl.Tasks[:i], tl.Tasks[i+1:]...)
+		}
+	}
+
+	return nil
+}
+
